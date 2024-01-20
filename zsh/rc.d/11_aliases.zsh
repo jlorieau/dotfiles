49d63c25bbe0d622232f2e7a6ee4abaa4f2df81f
J Lorieau <justin@lorieau.com>
2024-01-20 10:44:05 -0600

    Add 'll' alias

--- zsh/rc.d/11_aliases.zsh
+++ zsh/rc.d/11_aliases.zsh
@@ -2,3 +2,5 @@
 (( ${+commands[df]} )) && alias df="df --human-readable --print-type"
 (( ${+commands[du]} )) && alias du="du --human-readable --total"
 
+# listing aliases
+(( ${+commands[ls]} )) && alias alias ll='ls -alF'

06684bdf8dca757189be64394c8215d03554b0c2
J Lorieau <justin@lorieau.com>
2024-01-21 11:53:02 -0600

    Add syntax highlighting to cat/less/more

--- zsh/rc.d/11_aliases.zsh
+++ zsh/rc.d/11_aliases.zsh
@@ -1,6 +1,11 @@
-# Human file sizes
+# human file sizes
 (( ${+commands[df]} )) && alias df="df --human-readable --print-type"
 (( ${+commands[du]} )) && alias du="du --human-readable --total"
 
 # listing aliases
-(( ${+commands[ls]} )) && alias alias ll='ls -alF'
+(( ${+commands[ls]} )) && alias ll="ls -alF"
+
+# syntax highlighting
+(( ${+commands[pygmentize]} )) && alias cat="pygmentize -P style=gruvbox-dark"
+(( ${+commands[pygmentize]} )) && alias less='LESSOPEN="| pygmentize -O style=gruvbox-dark %s" less -M -R '
+(( ${+commands[pygmentize]} )) && alias more='LESSOPEN="| pygmentize -O style=gruvbox-dark %s" less -M -R '

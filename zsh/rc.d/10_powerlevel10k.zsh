a76dda5457d9bc4ef473450f57e9c3bb7d079144
J Lorieau <justin@lorieau.com>
2024-01-22 09:45:12 -0600

    Add prompt segment to show history state

--- zsh/rc.d/10_powerlevel10k.zsh
+++ zsh/rc.d/10_powerlevel10k.zsh
@@ -105,6 +105,7 @@ source "${ZDOTDIR}/plugins/powerlevel10k/powerlevel10k.zsh-theme"
     # battery               # internal battery
     # wifi                  # wifi speed
     # example               # example user-defined segment (see prompt_example function below)
+    history
   )
 
   # Defines character set used by powerlevel10k. It's best to let `p10k configure` set it for you.
@@ -1556,6 +1557,10 @@ source "${ZDOTDIR}/plugins/powerlevel10k/powerlevel10k.zsh-theme"
   # Type `p10k help segment` for documentation and a more sophisticated example.
   function prompt_example() {
     p10k segment -f 208 -i '⭐' -t 'hello, %n'
+  }
+
+  function prompt_history() {
+    [[ -z ${HISTFILE} ]] && p10k segment -f 28 -t '(anon)' || p10k segment -t ''
   }
 
   # User-defined prompt segments may optionally provide an instant_prompt_* function. Its job

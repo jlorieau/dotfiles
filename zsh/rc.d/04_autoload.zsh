45541599ef56e66900dd92cd5f8e8bf482659e38
J Lorieau <justin@lorieau.com>
2024-01-22 09:20:38 -0600

    Add load of add-zsh-hook function

--- zsh/rc.d/04_autoload.zsh
+++ zsh/rc.d/04_autoload.zsh
@@ -1,5 +1,8 @@
 # Autoload functions
 
+# Ensure add-zsh-hook is loaded, as it's used in rc files
+autoload -Uz add-zsh-hook
+
 # Custom personal functions (bottom)
 # ${fpath_custom} is set by env.d/03_paths.zsh
 autoload -U ${fpath_custom}/*(.:t)

bc00ff908b4b87fdf5ace8c3b29f1682d21a2dcf
J Lorieau <justin@lorieau.com>
2024-01-23 09:32:16 -0600

    Add aliases/exports for bat colorization

--- zsh/rc.d/12_colors.zsh
+++ zsh/rc.d/12_colors.zsh
@@ -1,3 +1,13 @@
 # Colors for different CLIs
 alias ls="ls --color=auto"
-(( ${+commands[grep]} )) && alias grep="grep --color=auto --binary-files=without-match --devices=skip"
+(( $+commands[grep] )) && alias grep="grep --color=auto --binary-files=without-match --devices=skip"
+
+# bat colorization
+if (( $+commands[bat] )); then
+	# -h/--help colorizer
+	alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+	alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
+
+	# man colorizer
+	export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+fi
